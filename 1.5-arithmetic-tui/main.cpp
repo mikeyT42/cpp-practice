57bfef9b6f3843d20d698e0b58c9d3b92a2c87ec
mikeyT42 <mrmojorisen0@gmail.com>
2026-01-03 23:58:49 -0500

    Wow, this is definitely cool!

--- 1.5-arithmetic-tui/main.cpp
+++ 1.5-arithmetic-tui/main.cpp
@@ -1,17 +1,53 @@
 #include <cmath>
 #include <cstdlib>
 
-#include "ftxui/screen/screen.hpp"
 #include "ftxui/dom/elements.hpp"
+#include "ftxui/screen/screen.hpp"
 
+int add(const int x, const int y);
+int subtract(const int x, const int y);
+int multiply(const int x, const int y);
+int divide(const int x, const int y);
+int remainder_of(const int x, const int y);
+int power(const int x, const int y);
+
+// -----------------------------------------------------------------------------
 int main() {
-  auto screen =
-      ftxui::Screen::Create(ftxui::Dimension::Full(), ftxui::Dimension::Full());
+  auto screen = ftxui::Screen::Create(ftxui::Dimension::Fixed(80),
+                                      ftxui::Dimension::Fixed(30));
 
-  auto window = ftxui::window(ftxui::text("Hi"), ftxui::text("me"));
-  
+  int x = 5;
+  int y = 2;
+
+  auto document =
+      ftxui::vbox({ftxui::hbox({ftxui::text("Adding " + std::to_string(x) +
+                                            " to " + std::to_string(y) + "."),
+                                ftxui::separator(),
+                                ftxui::text(std::to_string(add(x, y)))}) |
+                   ftxui::border});
+
+  auto window = ftxui::window(ftxui::text("Arithmetic"), document);
+
   ftxui::Render(screen, window);
   screen.Print();
 
   return EXIT_SUCCESS;
 }
+
+// -----------------------------------------------------------------------------
+int add(const int x, const int y) { return x + y; }
+
+// -----------------------------------------------------------------------------
+int subtract(const int x, const int y) { return x - y; }
+
+// -----------------------------------------------------------------------------
+int multiply(const int x, const int y) { return x * y; }
+
+// -----------------------------------------------------------------------------
+int divide(const int x, const int y) { return x / y; }
+
+// -----------------------------------------------------------------------------
+int remainder_of(const int x, const int y) { return x % y; }
+
+// -----------------------------------------------------------------------------
+int power(const int x, const int y) { return std::pow(x, y); }
