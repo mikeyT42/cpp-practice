142c1975eab800681aee7b114dd1d99fcd140e67
mikeyT42 <mrmojorisen0@gmail.com>
2026-01-03 23:00:42 -0500

    Ok, I am about to implement.

--- 1.5-arithmetic-tui/main.cpp
+++ 1.5-arithmetic-tui/main.cpp
@@ -1,13 +1,10 @@
-#include <ftxui/component/screen_interactive.hpp>
-#include <ftxui/component/component.hpp>
-#include <ftxui/component/component_options.hpp>
-#include <iostream>
- 
+#include <cmath>
+#include <cstdlib>
+
+#include "ftxui/component/screen_interactive.hpp"
+
 int main() {
-  using namespace ftxui;
- 
-  auto screen = ScreenInteractive::TerminalOutput();
-  auto button = Button("Click me", [] { std::cout << "Clicked!\n" << std::endl; });
- 
-  screen.Loop(button);
+  auto screen = ftxui::ScreenInteractive::Fullscreen();
+
+  return EXIT_SUCCESS;
 }
