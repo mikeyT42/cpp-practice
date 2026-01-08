5c3fac3ac1daee3ce26f64e2264839ee8fb81639
mikeyT42 <mrmojorisen0@gmail.com>
2026-01-07 23:16:32 -0500

    Wow, I got it! This is going to be very fun to implement the other programs.

--- 1.5-arithmetic-tui/main.cpp
+++ 1.5-arithmetic-tui/main.cpp
@@ -13,20 +13,62 @@ int power(const int x, const int y);
 
 // -----------------------------------------------------------------------------
 int main() {
-  auto screen = ftxui::Screen::Create(ftxui::Dimension::Fixed(80),
-                                      ftxui::Dimension::Fixed(30));
-
-  int x = 5;
-  int y = 2;
-
+  const int x = 5;
+  const int y = 2;
+  // clang-format off
   auto document =
-      ftxui::vbox({ftxui::hbox({ftxui::text("Adding " + std::to_string(x) +
-                                            " to " + std::to_string(y) + "."),
-                                ftxui::separator(),
-                                ftxui::text(std::to_string(add(x, y)))}) |
-                   ftxui::border});
+      ftxui::vbox({
+          ftxui::hbox({
+                  ftxui::text("Welcome") | ftxui::bold | ftxui::italic
+          }) | ftxui::flex | ftxui::center,
+          ftxui::separator(),
+          ftxui::hbox({
+                  ftxui::text("Adding " + std::to_string(x) + " to " +
+                          std::to_string(y) + "."),
+                  ftxui::separator(),
+                  ftxui::text(std::to_string(add(x, y)))
+          }) | ftxui::border,
+          ftxui::hbox({
+                  ftxui::text("Subtracting " + std::to_string(y) + " from " +
+                          std::to_string(x) + "."),
+                  ftxui::separator(),
+                  ftxui::text(std::to_string(subtract(x, y)))
+          }) | ftxui::border,
+          ftxui::hbox({
+                  ftxui::text("Multiplying " + std::to_string(x) + " by " +
+                          std::to_string(y) + "."),
+                  ftxui::separator(),
+                  ftxui::text(std::to_string(multiply(x, y)))
+          }) | ftxui::border,
+          ftxui::hbox({
+                  ftxui::text("Dividing " + std::to_string(x) + " by " +
+                          std::to_string(y) + "."),
+                  ftxui::separator(),
+                  ftxui::text(std::to_string(divide(x, y)))
+          }) | ftxui::border,
+          ftxui::hbox({
+                  ftxui::text("Getting the remainder of the division of " +
+                          std::to_string(x) + " by " + std::to_string(y) + "."),
+                  ftxui::separator(),
+                  ftxui::text(std::to_string(remainder_of(x, y)))
+          }) | ftxui::border,
+          ftxui::hbox({
+                  ftxui::text("Raising " + std::to_string(x) +
+                          " to the power of " + std::to_string(y) + "."),
+                  ftxui::separator(),
+                  ftxui::text(std::to_string(power(x, y)))
+          }) | ftxui::border,
+          ftxui::separator(),
+          ftxui::hbox({
+                  ftxui::text("Thank you") | ftxui::bold | ftxui::italic
+          }) | ftxui::flex | ftxui::center
+      });
+  // clang-format on
 
   auto window = ftxui::window(ftxui::text("Arithmetic"), document);
+
+  auto screen = ftxui::Screen::Create(ftxui::Dimension::Fixed(80),
+                                      ftxui::Dimension::Fit(window));
 
   ftxui::Render(screen, window);
   screen.Print();
