56f56b2acaa7bcef2ab3650cccab3388862dbff5
mikeyT42 <mrmojorisen0@gmail.com>
2026-01-10 01:11:19 -0500

    Trying a quit button; doesn't work.

--- 3.5-sentences/main.cpp
+++ 3.5-sentences/main.cpp
@@ -2,67 +2,127 @@
 #include <cstdlib>
 #include <string>
 
+#include "ftxui/component/component.hpp"
+#include "ftxui/component/screen_interactive.hpp"
 #include "ftxui/dom/elements.hpp"
 #include "ftxui/dom/table.hpp"
 #include "ftxui/screen/color.hpp"
-#include "ftxui/screen/screen.hpp"
 
 // -----------------------------------------------------------------------------
 typedef std::vector<std::string> table_row;
 
 // -----------------------------------------------------------------------------
+int keystrokes(std::string_view input);
+int alpha_chars(std::string_view input);
+int digit_chars(std::string_view input);
+int vowels(std::string_view input);
+
+// -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
 int main() {
-  std::vector<table_row> table_rows;
-  // table_rows.reserve(library.size() + 1);
-  table_rows.push_back({"Title", "Author", "Year", "Genre"});
+  const std::string PROMPT =
+      "Please input a sentence. If you want to exit, just hit enter.";
+  std::string input;
+  auto screen = ftxui::ScreenInteractive::Fullscreen();
 
-  /*
-  std::transform(library.cbegin(), library.cend(),
-                 std::back_inserter(table_rows),
-                 [](const book &b) { return b.to_table_row(); });
-  */
-
-  auto table = ftxui::Table({table_rows});
-  table.SelectAll().Border(ftxui::LIGHT);
-  table.SelectRow(0).Decorate(ftxui::bold);
-  table.SelectRow(0).SeparatorVertical(ftxui::LIGHT);
-  table.SelectRow(0).Border(ftxui::DOUBLE);
-
-  // Align right the "Release date" column.
-  table.SelectColumn(2).DecorateCells(ftxui::align_right);
-
-  // Select row from the second to the last.
-  auto content = table.SelectRows(1, -1);
-  // Alternate in between 3 colors.
-  content.DecorateCellsAlternateRow(color(ftxui::Color::DarkKhaki), 3, 0);
-  content.DecorateCellsAlternateRow(color(ftxui::Color::DarkSeaGreen), 3, 1);
-  content.DecorateCellsAlternateRow(color(ftxui::Color::DarkOrange), 3, 2);
+  auto sentence_input = ftxui::Input(&input);
+  auto component = ftxui::Container::Vertical({sentence_input});
+  auto quit_button = ftxui::Button("Quit", screen.ExitLoopClosure());
 
   // clang-format off
-  /*
-  auto renderer =
-      ftxui::vbox({
-          ftxui::hbox({
-                  ftxui::text("Welcome to the Sentence Data Aggregator")
-                    | ftxui::bold | ftxui::italic
-          }) | ftxui::flex | ftxui::center,
-          ftxui::separator(),
-          table.Render()| ftxui::center,
-          ftxui::separator(),
-          ftxui::hbox({
-                  ftxui::text("Thank you") | ftxui::bold | ftxui::italic
-          }) | ftxui::flex | ftxui::center
+  auto renderer = ftxui::Renderer(component, [&] {
+      auto table = ftxui::Table({
+          {"Keystrokes:", std::to_string(keystrokes(input))},
+          {"Alpha Characters:", std::to_string(alpha_chars(input))},
+          {"Numeric Characters:", std::to_string(digit_chars(input))},
+          {"Vowels:", std::to_string(vowels(input))},
       });
-  */
+      table.SelectAll().Border(ftxui::LIGHT);
+
+      // Select row from the second to the last.
+      auto content = table.SelectRows(0, -1);
+      // Alternate in between 3 colors.
+      content.DecorateCellsAlternateRow(color(ftxui::Color::DarkKhaki), 3, 0);
+      content.DecorateCellsAlternateRow(color(ftxui::Color::DarkSeaGreen), 3,
+              1);
+      content.DecorateCellsAlternateRow(color(ftxui::Color::DarkOrange), 3, 2);
+
+      return ftxui::window(ftxui::text("Sentence Data Aggregator"),
+          ftxui::vbox({
+              ftxui::hbox({
+                      ftxui::text("Welcome to the Sentence Data Aggregator")
+                        | ftxui::bold | ftxui::italic
+              }) | ftxui::flex | ftxui::center,
+              ftxui::separator(),
+              ftxui::hbox({
+                      ftxui::text(PROMPT) | ftxui::center,
+                      quit_button->Render() | ftxui::flex | ftxui::align_right
+              }),
+              sentence_input->Render(),
+              ftxui::separator(),
+              table.Render(),
+              ftxui::separator(),
+          })
+      );
+  });
   // clang-format on
 
-  auto window = ftxui::window(ftxui::text("Library"), document);
+  screen.Loop(renderer);
+}
 
-  auto screen = ftxui::Screen::Create(ftxui::Dimension::Full(),
-                                      ftxui::Dimension::Fit(window));
+// -----------------------------------------------------------------------------
+int keystrokes(std::string_view input) {
+  int total_keystrokes = 0;
 
-  ftxui::Render(screen, window);
-  screen.Print();
-  return EXIT_SUCCESS;
+  for (const auto &c [[maybe_unused]] : input)
+    total_keystrokes++;
+
+  return total_keystrokes;
+}
+
+// -----------------------------------------------------------------------------
+int alpha_chars(std::string_view input) {
+  int total_alpha = 0;
+
+  for (const auto &c : input) {
+    if (std::isalpha(static_cast<unsigned char>(c)))
+      total_alpha++;
+  }
+
+  return total_alpha;
+}
+
+// -----------------------------------------------------------------------------
+int digit_chars(std::string_view input) {
+  int total_digit = 0;
+
+  for (const auto &c : input) {
+    if (std::isdigit(static_cast<unsigned char>(c)))
+      total_digit++;
+  }
+
+  return total_digit;
+}
+
+// -----------------------------------------------------------------------------
+int vowels(std::string_view input) {
+  int total_vowels = 0;
+
+  for (const auto &c : input) {
+    switch (c) {
+    case 'a':
+    case 'A':
+    case 'e':
+    case 'E':
+    case 'i':
+    case 'I':
+    case 'o':
+    case 'O':
+    case 'u':
+    case 'U':
+      total_vowels++;
+    }
+  }
+
+  return total_vowels;
 }
